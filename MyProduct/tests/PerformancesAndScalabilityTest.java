4c70b7ece4fc1aa1e28a2dd8e31f1eb7db29732d
cbrun <cedric.brun@obeo.fr>
2011-11-07 13:45:02 +0100

    adding the example projects

---
+++ MyProduct/tests/PerformancesAndScalabilityTest.java
@@ -0,0 +1,11 @@
+import junit.framework.TestCase;
+
+import org.junit.Test;
+
+public class PerformancesAndScalabilityTest extends TestCase {
+
+  @Test public void nominalScenario() {
+    fail("You have to code this.");
+  }
+
+}
